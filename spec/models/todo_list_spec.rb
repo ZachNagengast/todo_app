06225bdbd6251f0438ea74266c6c5d086b70aac4
Zach Nagengast <zachnagengast09@gmail.com>
2016-04-09 21:20:18 -0700

    Finishing touches

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,44 @@ require 'spec_helper'
 
 describe TodoList do
   it { should have_many(:todo_items) }
+
+  describe "Setting hashids correct" do
+    it "encodes the hashid based on id" do
+
+    end
+
+    it "decodes the id based on hashid" do
+
+    end
+
+  end
+
+  describe "#has_completed_items?" do
+    let(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list") }
+
+    it "returns true with completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_completed_items?).to be_truthy
+    end
+
+    it "returns false when no completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_completed_items?).to be_falsey
+    end
+  end
+
+  describe "#has_completed_items?" do
+    let(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list") }
+
+    it "returns true with completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_incomplete_items?).to be_truthy
+    end
+
+    it "returns false when no completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_incomplete_items?).to be_falsey
+    end
+  end
+
 end
